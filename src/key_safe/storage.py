9ebef8476209651a967c0e1dc7203e90f17b72c1
Joel Miller <joelsamiller@gmail.com>
2025-02-15 19:18:46 +0000

    feat: add storage functions

---
+++ src/key_safe/storage.py
@@ -0,0 +1,26 @@
+import base64
+from typing import TextIO
+
+import pyperclip
+import tomlkit
+
+from key_safe import encryption
+
+
+def create_safe_file(file: TextIO) -> None:
+    safe = tomlkit.document()
+    safe["Key Safe"] = {}
+    tomlkit.dump(safe, file)
+
+
+def store_token(file: TextIO, name: str, token: bytes) -> None:
+    safe = tomlkit.load(file)
+    safe["Key Safe"][name] = base64.b64encode(token).decode()
+    file.seek(0)
+    tomlkit.dump(safe, file)
+
+
+def retrieve_token(file: TextIO, name: str, password: str) -> None:
+    safe = tomlkit.load(file)
+    token = base64.b64decode(safe["Key Safe"][name]).decode()
+    pyperclip.copy(encryption.decrypt(token, password))
